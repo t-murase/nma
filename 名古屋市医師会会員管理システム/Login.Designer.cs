784f70cb104166a99fca2cafbf9540e45e30ed1d
t-murase <toshihiko_murase@ishikai.nagoya>
2017-05-02 09:37:24 +0900

    txt

--- 名古屋市医師会会員管理システム/Login.Designer.cs
+++ 名古屋市医師会会員管理システム/Login.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.showMenu = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // showMenu
@@ -49,11 +50,21 @@
             this.textBox1.Size = new System.Drawing.Size(100, 19);
             this.textBox1.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(334, 223);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.showMenu);
             this.Name = "Login";
@@ -67,5 +78,6 @@
 
         private System.Windows.Forms.Button showMenu;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
